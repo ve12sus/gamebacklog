c7cf151162a8cdb5b10d03a3e11a2891e47d5e55
jeff <jeff@ve12sus.com>
2021-04-25 15:40:10 -0400

    removed temp files

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 
-describe('Bibliotom app test suite', function() {
+describe('Gamebacklog app test suite', function() {
   describe('#newFunction()', function() {
     it('test for this thing', function() {
       expect(newFunction(data)).to.deep.equal([]);
